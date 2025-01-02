855bd410cdac33c24b93044743717af98a0b1262
Anup kumar <anupkr72732gamil.com>
2025-01-02 22:26:57 +0530

    ochi project added navbar

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function LandingPage() {
+return (
+    <div className='w-full h-screen bg-zinc-900 pt-1'>
+        <div className="textstructure mt-40 px-20 ">
+            <div className="masker font-['Founders_Grotesk'] ">We create
+                <h1 className="uppercase text-9xl leading-none font-['Founders_Grotesk'">We create</h1>
+            </div>
+        </div>
+    </div>
+)
+}
+
+export default LandingPage
